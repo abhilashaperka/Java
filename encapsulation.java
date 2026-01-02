36d02f3ea6ed7bb6a82a382d323ce84a5af1a2fc
abhilashaperka <abhilashaperka@gmail.com>
2026-01-02 09:43:43 +0530

    encapsulation.java added successfully

---
+++ encapsulation.java
@@ -0,0 +1,29 @@
+class human{
+    private int age=11;
+    private String name="abhi";
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+      //private members are accessible only inside the class so here we are fetching the value of age and name
+    
+}
+public class encapsulation{
+    public static void main(String args[]){
+        human obj=new human();
+        obj.setAge(20);
+        obj.setName("abhilasha");
+        System.out.println(obj.getAge()+" "+obj.getName());
+
+    }
+}
