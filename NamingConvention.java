50e628b0ee578329e79519a98e108cd1976d3064
abhilashaperka <abhilashaperka@gmail.com>
2026-01-04 08:17:29 +0530

    NamingConvention.java added successfully

---
+++ NamingConvention.java
@@ -0,0 +1,16 @@
+class Naming{
+    //java follows the particular standards in naming the classes, var, methods,etc.
+        //java follows camel casing
+        // for classes and interfaces we use first letter as capital letter
+        int ID;// for constants use capital letters
+        String name;//var name and method name should be in small letters
+        public void showMyData(){
+            //if we join multiple word then we can use camel casing
+            System.out.println();
+        }
+}
+    public class NamingConvention {//in classname first letter should be capital
+    public static void main(String args[]){
+        
+    }
+}
