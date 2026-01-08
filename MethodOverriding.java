959b721f93c44e9676c622ef5cbfe79a8faeacb0
abhilashaperka <abhilashaperka@gmail.com>
2026-01-08 08:07:44 +0530

    MethodOverriding.java added successfully

---
+++ MethodOverriding.java
@@ -0,0 +1,22 @@
+class A{
+    public int add(int a, int b){
+        return a+b;
+    }
+}
+class B extends A{
+    public int add(int a,int b){
+        return a+b+1;
+    }
+}
+public class MethodOverriding {
+    public static void main(String args[]){
+        B obj=new B();
+        int r1=obj.add(5,6);
+        System.out.println(r1);
+
+    }
+    
+
+
+    
+}
