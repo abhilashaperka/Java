87f26e90df05d5d687ae9ee7e3375669466e1fde
abhilashaperka <abhilashaperka@gmail.com>
2026-01-08 07:41:57 +0530

    Super.java added successfully

--- Super.java
+++ Super.java
@@ -1,4 +1,4 @@
-class Parent{
+class Parent{//if we dont mention any class java will extends Object class
     public Parent(){
         super();
         System.out.println("it's a default parent class constructor");
@@ -11,11 +11,13 @@ class Parent{
 }
 class Child extends Parent{
     public Child(){
-        super();
+        super();//super(5);
         System.out.println("it's a default child class constructor");
     }
     public Child(int a){
-        super(a);//if we pass a parameter in super methos thn only it will call the parameterized constructor of the parent class otherwise it will call default constructor
+        
+        //super(a);//if we pass a parameter in super methos thn only it will call the parameterized constructor of the parent class otherwise it will call default constructor
+        this();//This can be used to call the parameterized constructor of the same class
         System.out.println("it's a child class parameterized constructor");
     }
 
@@ -23,7 +25,7 @@ class Child extends Parent{
 }
 public class Super {
     public static void main(String args[]){
-        Child obj=new Child(5);
+        Child obj=new Child(5);//Child obj=new Child();
 
     }
     
