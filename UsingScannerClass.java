bb0c53d7633851e1cc81b9fc6491ec0d9c12c7c9
abhilashaperka <abhilashaperka@gmail.com>
2026-02-05 09:01:23 +0530

    UsingScannerClass.java added successfully

---
+++ UsingScannerClass.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class UsingScannerClass {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int num=sc.nextInt();
+        System.out.println(num);
+        // System.out.println("enter your name");
+        // String s=sc.nextLine();  //for text input
+        // System.out.println(s);
+
+
+    }
+}
