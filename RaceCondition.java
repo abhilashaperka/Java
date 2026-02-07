a9e21962f63c67c929429ecfee3651920a193e8d
abhilashaperka <abhilashaperka@gmail.com>
2026-02-07 09:12:44 +0530

    RaceCondition.java added successfully

---
+++ RaceCondition.java
@@ -0,0 +1,34 @@
+class Counter{
+    int count;
+    public void increment(){
+        count++;
+    }
+}
+public class RaceCondition {
+    public static void main(String args[]) throws InterruptedException{
+
+        Counter c=new Counter();
+        Runnable obj=()->{
+            for(int i=0;i<100000;i++){
+                c.increment();
+            }
+        };
+        Runnable obj2=()->{
+            for(int i=0;i<100000;i++){
+                c.increment();
+            }
+        };
+        Thread t1=new Thread(obj);
+        Thread t2=new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        
+
+        System.out.println(c.count);
+
+    }   
+}
