c6503cbfdadb3e2c1981d6dee019ff90810d1773
abhilashaperka <abhilashaperka@gmail.com>
2026-02-14 08:06:58 +0530

    MapFilterReduce.java added successfully

---
+++ MapFilterReduce.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+public class MapFilterReduce{
+     public static void main(String args[]){
+        List<Integer> nums=Arrays.asList(5,7,2,6,9,4);
+        // Stream<Integer> s1=nums.stream();
+        // Stream<Integer> s2=s1.filter(n->n%2==0);
+        // Stream<Integer> s3=s2.map(n->n*2);
+        // int result=s3.reduce(0,(c,e)->c+e);
+        int result=nums.stream()
+                       .filter(n->n%2==0)
+                       .map(n->n*2)
+                       .reduce(0,(c,e)->c+e);
+        System.out.println(result);
+
+    }
+}
+
